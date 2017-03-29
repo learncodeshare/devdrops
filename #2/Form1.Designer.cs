5b12dcc1b8dddd59120efdf18d6da4c6bc39b342
Daniel Ancines <Daniel Ancines>
2017-03-29 14:42:05 -0300

    Dev Drops #2

---
+++ #2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace LCS.DevDrops2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exemple1Button = new System.Windows.Forms.Button();
+            this.Exemple2Button = new System.Windows.Forms.Button();
+            this.Exemple3Button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Exemple1Button
+            // 
+            this.Exemple1Button.Location = new System.Drawing.Point(13, 13);
+            this.Exemple1Button.Name = "Exemple1Button";
+            this.Exemple1Button.Size = new System.Drawing.Size(668, 23);
+            this.Exemple1Button.TabIndex = 0;
+            this.Exemple1Button.Text = "Exemplo 1";
+            this.Exemple1Button.UseVisualStyleBackColor = true;
+            this.Exemple1Button.Click += new System.EventHandler(this.Exemple1Button_Click);
+            // 
+            // Exemple2Button
+            // 
+            this.Exemple2Button.Location = new System.Drawing.Point(12, 42);
+            this.Exemple2Button.Name = "Exemple2Button";
+            this.Exemple2Button.Size = new System.Drawing.Size(668, 23);
+            this.Exemple2Button.TabIndex = 0;
+            this.Exemple2Button.Text = "Exemplo 2";
+            this.Exemple2Button.UseVisualStyleBackColor = true;
+            this.Exemple2Button.Click += new System.EventHandler(this.Exemple2Button_Click);
+            // 
+            // Exemple3Button
+            // 
+            this.Exemple3Button.Location = new System.Drawing.Point(12, 71);
+            this.Exemple3Button.Name = "Exemple3Button";
+            this.Exemple3Button.Size = new System.Drawing.Size(668, 23);
+            this.Exemple3Button.TabIndex = 0;
+            this.Exemple3Button.Text = "Exemplo 3";
+            this.Exemple3Button.UseVisualStyleBackColor = true;
+            this.Exemple3Button.Click += new System.EventHandler(this.Exemple3Button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resultado";
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(13, 118);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.ReadOnly = true;
+            this.ResultTextBox.Size = new System.Drawing.Size(667, 20);
+            this.ResultTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 160);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Exemple3Button);
+            this.Controls.Add(this.Exemple2Button);
+            this.Controls.Add(this.Exemple1Button);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Learn. Code. Share! DevDrops2 - Out Variables";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exemple1Button;
+        private System.Windows.Forms.Button Exemple2Button;
+        private System.Windows.Forms.Button Exemple3Button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ResultTextBox;
+    }
+}
+
---
+++ #2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LCS.DevDrops2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Exemple1Button_Click(object sender, EventArgs e)
+        {
+            //Guid novoId;
+            var valor = "123456789456123";
+
+            if (Guid.TryParse(valor, out Guid novoId))
+                this.ResultTextBox.Text = string.Format("Conversão com Sucesso, valor: {0} é um Guid válido", novoId.ToString()); 
+            else
+                this.ResultTextBox.Text = string.Format("Erro na Conversão, valor: {0} inválido", valor);
+        }
+
+        private void Exemple2Button_Click(object sender, EventArgs e)
+        {
+            //int novoNumero;
+            var valor = "123456";
+
+            if (int.TryParse(valor, out int novoNumero))
+                this.ResultTextBox.Text = string.Format("Conversão com Sucesso, valor: {0} é um inteiro", novoNumero);
+            else
+                this.ResultTextBox.Text = string.Format("Erro na Conversão, valor: {0} inválido", valor);
+        }
+
+        private void Exemple3Button_Click(object sender, EventArgs e)
+        {
+            //int total;
+            var v1 = 10;
+            var v2 = 0;
+
+            if (this.TryDivisao(v1, v2, out int total))
+                this.ResultTextBox.Text = string.Format("Total ({0} / {1}): {2}", v1, v2, total);
+            else
+                this.ResultTextBox.Text = "Erro na Divisão, valores inválidos";
+        }
+
+        private bool TryDivisao(int v1, int v2, out int total)
+        {
+            try
+            {
+                total = v1 / v2;
+                return true;
+            }
+            catch
+            {
+                total = -1;
+                return false;
+            }
+        }
+    }
+}
