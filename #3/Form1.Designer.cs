ca7abeae7152a3d17044b771bd1bcd59fc29f030
Daniel Ancines <Daniel Ancines>
2017-04-05 15:01:01 -0300

    Pattern Matching

---
+++ #3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace LCS.DevDrops3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.SwitchButton = new System.Windows.Forms.Button();
+            this.SwitchTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(13, 13);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(259, 23);
+            this.ExecuteButton.TabIndex = 0;
+            this.ExecuteButton.Text = "Executar";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // SwitchButton
+            // 
+            this.SwitchButton.Location = new System.Drawing.Point(12, 42);
+            this.SwitchButton.Name = "SwitchButton";
+            this.SwitchButton.Size = new System.Drawing.Size(259, 23);
+            this.SwitchButton.TabIndex = 0;
+            this.SwitchButton.Text = "Switch";
+            this.SwitchButton.UseVisualStyleBackColor = true;
+            this.SwitchButton.Click += new System.EventHandler(this.SwitchButton_Click);
+            // 
+            // SwitchTextBox
+            // 
+            this.SwitchTextBox.Location = new System.Drawing.Point(13, 72);
+            this.SwitchTextBox.Name = "SwitchTextBox";
+            this.SwitchTextBox.Size = new System.Drawing.Size(258, 20);
+            this.SwitchTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 105);
+            this.Controls.Add(this.SwitchTextBox);
+            this.Controls.Add(this.SwitchButton);
+            this.Controls.Add(this.ExecuteButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.Button SwitchButton;
+        private System.Windows.Forms.TextBox SwitchTextBox;
+    }
+}
+
---
+++ #3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LCS.DevDrops3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            object a = "10";
+            if (!(a is string b)) return;
+            MessageBox.Show(b);
+        }
+
+        private void SwitchButton_Click(object sender, EventArgs e)
+        {
+            this.SwitchTest(this.SwitchTextBox.Text);
+        }
+
+        private void SwitchTest(object obj)
+        {
+            if (!(obj is null))
+            {
+                int i;
+                if (int.TryParse(obj.ToString(), out i))
+                    MessageBox.Show("Valor é um inteiro " + i);
+            }
+
+            switch (obj)
+            {
+                case null:
+                    MessageBox.Show("Nada foi digitado");
+                    break;
+                case string a when int.TryParse(a, out int b):
+                    MessageBox.Show("Valor é um inteiro " + b);
+                    break;
+                case int a:
+                    MessageBox.Show("integer " + a);
+                    break;
+                case double a:
+                    MessageBox.Show("double " + a);
+                    break;
+                default:
+                    MessageBox.Show("Default");
+                    break;
+            }
+        }
+    }
+}
